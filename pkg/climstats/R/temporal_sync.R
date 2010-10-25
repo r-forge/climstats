86caf25d6ec08fdf29c6fce9c008acd3bbbff1df
am_umt (Alison Mynsberge) <alison.mynsberge@umontana.edu>
2010-10-25 18:50:06 +0000

    Added month/year synchronization
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/climstats@72 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/climstats/R/temporal_sync.R
+++ pkg/climstats/R/temporal_sync.R
@@ -78,4 +78,48 @@ temporal_sync_raster <- function(unsynced,reference,synctype="by.month",unsynced
 			return(synced_stack)
 		}	
 	}
+	if(synctype=="by.year.month")
+	{
+		if(verbose)
+		{
+			print("Syncing by year & month...")
+		}
+		unsynced_mo_yr=format(unsynced_dates,"%Y-%m")
+		reference_mo_yr=format(reference_dates,"%Y-%m")
+		
+		unsynced_mo_yr_unique=unique(unsynced_mo_yr)
+		if(length(unsynced_mo_yr_unique) != length(unsynced_mo_yr))
+		{
+			print('synctype="by.year.month" requires a single entry per month per year in the unsynced file')
+			return()
+		}
+		
+		synccheck=unique(reference_mo_yr %in% unsynced_mo_yr)
+		if(length(synccheck) > 1 | synccheck[1]==FALSE)
+		{
+			print("Reference contains unsyncable dates, does unsynced file include all reference months and years?")
+			return()
+		}
+		
+		unsynced_idx=1:(length(unsynced_mo_yr))
+		reference_unsynced_idx=vector(mode="numeric",length=length(reference_mo_yr))
+		for(i in 1:length(reference_mo_yr))
+		{
+			reference_unsynced_idx[i]=unsynced_idx[unsynced_mo_yr %in% reference_mo_yr[i]]		
+		}
+		if(class(unsynced)=="character")
+		{
+			synced_fname_list=vector(mode="list",length=length(reference_months))
+			synced_bands_list=vector(mode="list",length=length(reference_months))
+			for(i in 1:length(reference_months))
+			{
+				synced_fname_list[[i]]=unsynced
+				synced_bands_list[[i]]=reference_unsynced_idx[i]
+			}
+			synced_stack=stack(synced_fname_list,bands=synced_bands_list)
+			synced_stack@zvalue=reference@zvalue
+			
+			return(synced_stack)
+		}	
+	}
 }
