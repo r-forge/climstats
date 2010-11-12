f0350ec976b77fe62b9badcab9b3c2e45f98d9ec
jgrn307 (Dr. ) <jgreenberg@unr.edu>
2010-11-12 03:45:16 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/climstats@78 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/climstats/R/spatial_sync.R
+++ pkg/climstats/R/spatial_sync.R
@@ -3,7 +3,7 @@
 # Author: jonathan
 ###############################################################################
 
-spatial_sync_raster <- function(unsynced,reference,method="ngb",verbose=FALSE,crop_only=FALSE)
+spatial_sync_raster <- function(unsynced,reference,method="ngb",verbose=FALSE)
 {
 	new_projection=projection(reference)
 	old_projection=projection(unsynced)
@@ -14,20 +14,18 @@ spatial_sync_raster <- function(unsynced,reference,method="ngb",verbose=FALSE,cr
 	if(new_projection!=old_projection | new_res[1] != old_res[1] | new_res[2] != old_res[2])
 	{
 		pr_extent=projectExtent(unsynced, new_projection)
+		# We need to fix the extent
+		pr_extent <- setExtent(pr_extent,extent(reference))
 		res(pr_extent)=res(reference)
 		pr <- projectRaster(unsynced, pr_extent,method=method)
 	}
 	
-	if(crop_only)
-	{
-		synced_raster=crop(pr,reference)
-    } else
-	{
-		synced_raster=crop(expand(pr,reference),reference)
-	}
+	expanded_raster=expand(pr,reference)
+	synced_raster=crop(expanded_raster,reference)
+
 
 	# This in theory shouldn't be neccessary...
-	extent(synced_raster)=extent(reference)
+#	extent(synced_raster)=extent(reference)
 	
 	return(synced_raster)
 	
