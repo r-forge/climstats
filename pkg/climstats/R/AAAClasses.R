966108531bf11849e7179567f75b23b9b20666b3
jgrn307 (Dr. ) <jgreenberg@unr.edu>
2011-01-18 20:42:24 +0000

    deleted AAAClasses.R for now
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/climstats@90 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/climstats/R/AAAClasses.R
+++
@@ -1,8 +0,0 @@
-# TODO: Add comment
-# 
-# Author: jonathan
-###############################################################################
-
-
-	setClass("SpatialPointsList",representation(coords="SpatialPoints",list="list"))
-#	setClass("station",representation(id="character",measurement="character"),contains="spZoo")
